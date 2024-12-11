ab0e3529e58062eb64959146876ced14ce48d368
Geoo Chi <geoooochi@gmail.com>
2024-12-12 01:20:46 +0800

    feat: add Button

---
+++ src/layout/Button.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+
+export default function ButtonDemo() {
+  return (
+    <div className='flex flex-col gap-4 justify-center items-center h-screen'>
+      <Button>Button</Button>
+      <Button variant='secondary'>Secondary</Button>
+      <Button variant='outline'>Outline</Button>
+      <Button variant='ghost'>Ghost</Button>
+      <Button variant='link'>Link</Button>
+    </div>
+  )
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,12 @@
 import Home from './layout/Home'
-import SidebarDemo from './layout/Sidebar'
 import AccordionDemo from './layout/Accordion'
 import AlertDemo from './layout/Alert'
 import AlertDialogDemo from './layout/AlertDialog'
 import AvatarDemo from './layout/Avatar'
 import BadgeDemo from './layout/Badge'
 import BreadcrumbDemo from './layout/Breadcrumb'
+import ButtonDemo from './layout/Button'
+import SidebarDemo from './layout/Sidebar'
 import PageDemo from './layout/Page'
 
 interface Route {
@@ -16,13 +17,14 @@ interface Route {
 
 const routes: Route[] = [
   { path: '/', name: 'Home', element: Home },
-  { path: '/sidebar', name: 'Sidebar', element: SidebarDemo },
   { path: '/accordion', name: 'Accordion', element: AccordionDemo },
   { path: '/alert', name: 'Alert', element: AlertDemo },
   { path: '/alert-dialog', name: 'AlertDialog', element: AlertDialogDemo },
   { path: '/avatar', name: 'Avatar', element: AvatarDemo },
   { path: '/badge', name: 'Badge', element: BadgeDemo },
   { path: '/breadcrumb', name: 'Breadcrumb', element: BreadcrumbDemo },
+  { path: '/button', name: 'Button', element: ButtonDemo },
+  { path: '/sidebar', name: 'Sidebar', element: SidebarDemo },
   { path: '/page', name: 'Page', element: PageDemo },
 ]
 
