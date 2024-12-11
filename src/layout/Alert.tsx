cd81b5430cd681a62df175bc19c2105ed0567c2e
Geoo Chi <geoooochi@gmail.com>
2024-12-12 00:27:51 +0800

    feat: add alert

---
+++ src/layout/Alert.tsx
@@ -0,0 +1,23 @@
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+import { Terminal } from 'lucide-react'
+
+export default function MyAlert() {
+  return (
+    <div className='flex flex-col gap-6 justify-center items-center h-screen'>
+      <Alert className='w-1/2'>
+        <Terminal className='h-4 w-4' />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components and dependencies to your app using the cli.
+        </AlertDescription>
+      </Alert>
+      <Alert className='w-1/2' variant='destructive'>
+        <Terminal className='h-4 w-4' />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components and dependencies to your app using the cli.
+        </AlertDescription>
+      </Alert>
+    </div>
+  )
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import Home from './layout/Home'
 import Sidebar from './layout/Sidebar'
 import MyAccordion from './layout/Accordion'
+import MyAlert from './layout/Alert'
 import Page01 from './layout/Page01'
 import Page02 from './layout/Page02'
 
@@ -14,6 +15,7 @@ const routes: Route[] = [
   { path: '/', name: 'Home', element: Home },
   { path: '/sidebar', name: 'Sidebar', element: Sidebar },
   { path: '/accordion', name: 'Accordion', element: MyAccordion },
+  { path: '/alert', name: 'Alert', element: MyAlert },
   { path: '/page01', name: 'Page01', element: Page01 },
   { path: '/page02', name: 'Page02', element: Page02 },
 ]
