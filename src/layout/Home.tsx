189795c79d909fbcb8cfb960748045c4b327c112
Geoo Chi <geoooochi@gmail.com>
2024-12-12 00:23:22 +0800

    feat: add accordion

--- src/layout/Home.tsx
+++ src/layout/Home.tsx
@@ -6,17 +6,21 @@ export default function Home() {
   return (
     <div className='flex justify-center items-center h-screen'>
       <div className='flex flex-col gap-2'>
-        {routes.map(route => (
-          <Link
-            className={cn(
-              'text-center px-10 py-4 bg-gray-200',
-              ' hover:bg-blue-500 hover:text-white rounded-md'
-            )}
-            to={route.path}
-          >
-            {route.name}
-          </Link>
-        ))}
+        {routes.map(
+          route =>
+            route.name !== 'Home' && (
+              <Link
+                className={cn(
+                  'text-center px-10 py-4 bg-gray-200',
+                  ' hover:bg-blue-500 hover:text-white rounded-md'
+                )}
+                key={route.path}
+                to={route.path}
+              >
+                {route.name}
+              </Link>
+            )
+        )}
       </div>
     </div>
   )
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import Home from './layout/Home'
 import Sidebar from './layout/Sidebar'
+import MyAccordion from './layout/Accordion'
 import Page01 from './layout/Page01'
 import Page02 from './layout/Page02'
 
@@ -12,6 +13,7 @@ interface Route {
 const routes: Route[] = [
   { path: '/', name: 'Home', element: Home },
   { path: '/sidebar', name: 'Sidebar', element: Sidebar },
+  { path: '/accordion', name: 'Accordion', element: MyAccordion },
   { path: '/page01', name: 'Page01', element: Page01 },
   { path: '/page02', name: 'Page02', element: Page02 },
 ]
